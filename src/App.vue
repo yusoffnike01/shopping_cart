611b6f3c732c1f2e7785f83c30e220bf06ea0037
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-02-01 00:26:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="container">
+    <div class="row">
+<div class="col-md-7">
+<div class="row">
+
+
+  <div :key="product.id" class="col-md-6" v-for="product in products">
+    <product :isIncart=isIncart(product) v-on:add-to-cart="addcart(product)" :product="product"></product>
+  </div>
+</div>
+<div class="col-md-5 my-5">
+<cart  v-on:pay="pay()" v-on:remove-cart="removecart($event)" :items="cart"></cart>
+
+</div>
+
+</div>
+
+    </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import products from '@/products.json';
+import Product from '@/components/Product.vue';
+import cart from '@/components/cart.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+
+
+Product,
+cart
+
+
+  },
+  data(){
+    return{
+      products,
+      cart:[]
+      
+       
+    }
+  },
+  methods:
+  {
+    addcart(product)
+    {
+      this.cart.push(product)
+    },
+
+    isIncart(product)
+    {
+      const item=this.cart.find(item=>item.id===product.id)
+   
+   if(item)
+   {
+     return true
+   }
+   return false
+   
+   
+   },
+   removecart(product)
+   {
+     this.cart=this.cart.filter(item=> item.id!==product.id)
+     console.log('wsww')
+   },
+   pay()
+   {
+     this.cart=[]
+     alert('Shopping Completed')
+   }
   }
+   
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped >
+body{
+  background-color: aliceblue;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="card my-5">
+
+<div class="card-body">
+
+    <h5 class="text-center card-title"> {{product.title}}</h5>
+<p class="text-center text-muted card-text display-4">${{Number(product.price).toFixed(2)}}</p>
+<button  :disable="isIncart" @click="$emit('add-to-cart',product)" class="btn btn-primary form-control">{{isIncart? 'Added to cart' : 'Add to cart'}}</button>
+
+
+</div>
+
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    props:['product','isIncart']
+
+}
+</script>
